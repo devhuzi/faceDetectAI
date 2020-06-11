c9df499d4b2193688d2b2d7fc566d3e757fda994
DevHuzi <hux3fa@gmail.com>
2020-06-11 10:52:24 +1000

    Updates

---
+++ precache-manifest.6db43e207f43fd706ea94ef15c7cb981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f386e14b8f5a044f722857e172f98b85",
+    "url": "/faceDetectAI/index.html"
+  },
+  {
+    "revision": "262a75681aba5448b769",
+    "url": "/faceDetectAI/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b6318f8cfcf66e768511",
+    "url": "/faceDetectAI/static/css/main.00272af9.chunk.css"
+  },
+  {
+    "revision": "262a75681aba5448b769",
+    "url": "/faceDetectAI/static/js/2.03fad486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceDetectAI/static/js/2.03fad486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6318f8cfcf66e768511",
+    "url": "/faceDetectAI/static/js/main.941a7ac4.chunk.js"
+  },
+  {
+    "revision": "cd73d9cd68f0c848b0aa",
+    "url": "/faceDetectAI/static/js/runtime-main.f83c4c46.js"
+  }
+]);
